b9b25b8b1ecc3f90c002463e0c6722f22d60633c
arturovolpe@gmail.com <arturovolpe@gmail.com@85402f31-8331-1f9c-1146-eb63fd9d0980>
2012-04-04 01:19:57 +0000

    Tag de la primera entrega

---
+++ tags/Entrega1/yapp/__init__.py
@@ -0,0 +1,23 @@
+from .models import DBSession
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+import os
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    config = Configurator(settings=settings)
+#    config.add_static_view('static', 'static', cache_max_age=3600)
+#    config.add_route('home', '/')
+    config.add_static_view('static', os.path.join(here, 'static'))
+    config.add_route('main', '/main')
+    config.add_route('crearProyecto', '/crearProyecto')
+    config.scan()
+#    config.scan("views")
+    return config.make_wsgi_app()
+
