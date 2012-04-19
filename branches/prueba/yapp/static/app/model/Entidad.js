e99e063f87a73831e2e90720c9ed0a8afb7c556b
ugilez1 <ugilez1@85402f31-8331-1f9c-1146-eb63fd9d0980>
2012-04-19 20:59:14 +0000

    branch temporal

--- branches/prueba/yapp/static/app/model/Entidad.js
+++
@@ -1,6 +0,0 @@
-Ext.define('YAPP.model.Entidad', {
-    extend: 'Ext.data.Model',
-// fields: [{nYAPPe: 'nYAPPe', type: 'int', }, 'email', 'estado', 'esFinal']
-    fields: ['_id', '_nombre'],
-
-});
