802a259c77a0ff3bb0524c0dd36b22a5b39158ed
ugilez1 <ugilez1@gmail.com>
2012-04-20 02:29:07 +0000

    Merge con la Rama Gonzalez

--- yapp/yapp/models/roles/rol_final.py
+++ yapp/yapp/models/roles/rol_final.py
@@ -14,13 +14,15 @@ from yapp.models.roles.rol import Rol
 class RolFinal (Rol):
     __tablename__ = "rol_final"
     __mapper_args__ = {'polymorphic_identity': 'rolfinal'}
-    _usuario = Column(String, nullable=True)
     _email = Column(String, nullable=True)
+    _password = Column(String, nullable=False)
     id = Column(Integer, ForeignKey('rol._id'), primary_key=True)
     
-    def __init__(self, usuario, email):
-        self._email = email;
-        self._usuario = usuario;
+    def __init__(self, nombre, estado,email, password):
+        self._nombre = nombre
+        self._estado = estado
+        self._email = email
+        self._password = password
         self._esFinal = True
         
     
--- yapp/yapp/static/crearProyecto.js
+++ yapp/yapp/static/crearProyecto.js
@@ -1,50 +1,166 @@
+//Ext.require([
+//    'Ext.form.*',
+//    'Ext.layout.container.Column',
+//    'Ext.tab.Panel',
+//    'Ext.QuickTip'
+//]);
+//
+//Ext.onReady(function(){
+//
+//	Ext.QuickTips.init();
+//    var formulario = new Ext.FormPanel({ 
+//        frame:true,
+//        title: 'Crear proyecto',
+//        bodyStyle:'padding:10px',
+//        width: 1000,
+//        fieldDefaults: {
+//            msgTarget: 'side', //para que muestre el error al costado
+//            labelWidth: 75
+//        },
+//        defaultType: 'textfield',
+//        defaults: {
+//            anchor: '100%'
+//        },
+//
+//        items: [{
+//            fieldLabel: 'Nombre del proyecto',
+//            name: 'nombre',
+//            allowBlank:false
+//        },{
+//            fieldLabel: 'Autor del proyecto',
+//            name: 'autor',
+//            allowBlank:false
+//        }],
+//
+////        buttons: [{
+////            text: 'Guardar',
+////            href: 'main',
+////            handler: function(){
+////                this.up('form').getForm().submit({
+////                	url: 'crearProyecto',
+////                    submitEmptyText: false,
+////                });
+////            }
+////        },{
+////            text: 'Cancelar',
+////            href: 'main'thod:'POST',  
+////            waitMsg:'Ingresando...',
+////            
+////        }]
+//        buttons:[{ 
+//            text:'Crear',
+//            formBind: true,	 
+//            
+//            handler:function(){ 
+//            	formulario.getForm().submit({ 
+//                    method:'POST',  
+//                    waitMsg:'Creando...',
+//
+//
+//                    success:function(){ 
+//                    	Ext.Msg.alert('Estado', 'Acceso correcto!', function(btn, text){
+//                    	if (btn == 'ok'){
+//                    		var redirect = 'main'; 
+//                    		window.location = redirect;
+//	                        }
+//				        });
+//                	},
+//                	failure:function(form, action){ 
+//                        if(action.failureType == 'server'){                       
+//                            Ext.Msg.alert('Error al ingresar!', obj.errors.reason); 
+//                        }else{ 
+//                            Ext.Msg.alert('Error!', 'No se puede conectar al servidor : ' + action.response.responseText); 
+//                        } 
+//                        formulario.getForm().reset(); 
+//                    } 
+//                }); 
+//            } 
+//        }] 
+//    });
+//
+//    var win = new Ext.Window({
+//        layout:'fit',
+//        width:500,
+//        height:200,
+//        closable: false,
+//        resizable: false,
+//        plain: true,
+//        border: false,
+//        items: [formulario]
+//	});
+//    win.show()
+//});
 Ext.require([
     'Ext.form.*',
     'Ext.layout.container.Column',
-    'Ext.tab.Panel'
+    'Ext.tab.Panel',
+    'Ext.QuickTip'
 ]);
 
+
 Ext.onReady(function(){
-
-
-    var formulario = Ext.create('Ext.form.Panel', {
-        frame:true,
-        title: 'Crear proyecto',
-        bodyStyle:'padding:10px',
-        width: 1000,
-        fieldDefaults: {
-            msgTarget: 'side', //para que muestre el error al costado
-            labelWidth: 75
-        },
-        defaultType: 'textfield',
-        defaults: {
-            anchor: '100%'
-        },
-
-        items: [{
-            fieldLabel: 'Nombre del proyecto',
-            name: 'nombre',
-            allowBlank:false
-        },{
-            fieldLabel: 'Autor del proyecto',
-            name: 'autor',
-            allowBlank:false
-        }],
-
-        buttons: [{
-            text: 'Guardar',
-            handler: function(){
-                this.up('form').getForm().submit({
-                	url: 'crearProyecto',
-                    submitEmptyText: false,
-                });
-            }
-            
-        },{
-            text: 'Cancelar',
-            href: 'main'
-        }]
+    Ext.QuickTips.init();
+    var login = new Ext.FormPanel({ 
+    	
+        labelWidth:80,
+        url:'crearProyecto', 
+        frame:true, 
+        title:'Crear Proyecto', 
+        defaultType:'textfield',
+        monitorValid:true,
+			items:[{ 
+                fieldLabel:'Nombre', 
+                name:'nombre', 
+                allowBlank:false 
+            },{ 
+                fieldLabel:'Autor', 
+                name:'autor', 
+                allowBlank:false 
+            }],
+   
+        buttons:[{ 
+                text:'Ingresar',
+                formBind: true,	 
+                
+                handler:function(){ 
+                    login.getForm().submit({ 
+                        method:'POST',  
+                        waitMsg:'Ingresando...',
+ 
+ 
+                        success:function(){ 
+                        	Ext.Msg.alert('Estado', 'Proyecto creado!', function(btn, text){
+                        	if (btn == 'ok'){
+                        		var redirect = 'main'; 
+                        		window.location = redirect;
+		                        }
+					        });
+                    	},
+                    	failure:function(form, action){ 
+                            if(action.failureType == 'server'){                       
+                                Ext.Msg.alert('Error al crear proyecto!', obj.errors.reason); 
+                            }else{ 
+                                Ext.Msg.alert('Error!', 'No se puede conectar al servidor : ' + action.response.responseText); 
+                            } 
+                            login.getForm().reset(); 
+                        } 
+                    }); 
+                } 
+            }] 
     });
-
-    formulario.render(document.body);
+ 
+ 
+      
+    var win = new Ext.Window({
+        layout:'fit',
+        width:300,
+        height:150,
+        closable: false,
+        resizable: false,
+        plain: true,
+        border: false,
+        items: [login]
+	});
+	win.show();
+//    win.render(document.body);
 });
