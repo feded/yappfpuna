378f00ac20f6e98e3d1b5f7b72a75b851491a211
ugilez1@gmail.com <ugilez1@gmail.com>
2012-05-01 12:50:09 +0000

    Se agrega crear recurso

--- gonzalez/yapp/yapp/static/app/controller/Recursos.js
+++ gonzalez/yapp/yapp/static/app/controller/Recursos.js
@@ -2,11 +2,38 @@ Ext.define('YAPP.controller.Recursos', {
     extend: 'Ext.app.Controller',
 
 	views: [
-		'recurso.ListarRecurso'
+		'recurso.ListarRecurso', 'recurso.NuevoRecurso'
 		],
 	stores:['Recursos'],
 	models:['Recurso'],
     init: function() {
         console.log('Recursos');
-    }
+        this.control({
+            	'nuevorecurso button[action=guardar]': {
+            		click: this.guardarNuevoRecurso
+            	},
+            	
+            	'listarrecurso button[action=crear]':{
+            		click: this.crearRecurso
+            	},
+        });
+    },
+    
+    guardarNuevoRecurso: function(button){
+		var win = button.up('window');
+		var form = win.down('form');
+		var record = form.getRecord();
+		var values = form.getValues();
+		record.set(values);	
+		win.close();
+		this.getRecursosStore().insert(0, record);
+	},
+	
+	crearRecurso: function(button){
+		var view = Ext.widget('nuevorecurso');
+        var recurso = new YAPP.model.Recurso();
+        		
+		var win = button.up('grid');
+		view.down('form').loadRecord(recurso);
+	}
 });
