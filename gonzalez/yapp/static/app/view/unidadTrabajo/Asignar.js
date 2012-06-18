061625b6bc1c30dcc1d0561a7ff1104563bb6884
ugilez1@gmail.com <ugilez1@gmail.com>
2012-06-18 14:59:59 +0000

    Mejoras en cuanto a la visualizacion del costo en recursos

--- gonzalez/yapp/static/app/view/unidadTrabajo/Asignar.js
+++ gonzalez/yapp/static/app/view/unidadTrabajo/Asignar.js
@@ -13,81 +13,55 @@ Ext.define('YAPP.view.unidadTrabajo.Asignar', {
 	
 	
 	initComponent : function() {
-		this.items =[
-//		 				{
-//							xtype : 'form',
-//				//			type : 'hbox',
-//							items : [ 
-//										{
-//											xtype : 'textfield',
-//											name : '_nombre',
-//											fieldLabel : 'Nombre',
-//											allowBlank : false
-//										},
-//										{
-//											xtype : 'textfield',
-//											name : '_descripcion',
-//											fieldLabel : 'Descripcion',
-//											allowBlank : false
-//										}
-//									],
-//						},
-						{
-							xtype : 'container',
-							width : 650,
-							height : 300,
-							layout : 
-									{
-										type : 'hbox',
-										align : 'stretch',
-										padding : 5
-									},
-							defaults : 
-							{
-								flex : 1
-							},
-							items :
-									[ 
-										{
-											xtype : 'gridpanel',
-											name : 'firstGrid',
-											height : 100,
-											store : Ext.create('YAPP.store.Recursos'),
-											multiSelect : true,
-											viewConfig : 
-														{
-															plugins :
-																	{
-																		ptype : 'gridviewdragdrop',
-																		dragGroup : 'firstGridDDGroup',
-																		dropGroup : 'secondGridDDGroup'
-																	},
-														},
-											columns : columnas,
-											// stripeRows : true,
-											title : 'Recursos Disponibles',
-											margins : '0 2 0 0'
-										},
-										{
-											xtype : 'gridpanel',
-											name : 'secondGrid',
-											store : Ext.create('YAPP.store.Recursos'),
-											height : 100,
-											viewConfig : {
-												plugins : {
-													ptype : 'gridviewdragdrop',
-													dragGroup : 'secondGridDDGroup',
-													dropGroup : 'firstGridDDGroup'
-												},
-											},
-											columns : columnas,
-											// stripeRows : true,
-											title : 'Recursos asignados',
-											margins : '0 0 0 3'
-										} 
-									]
-						}
-					]
+		this.items =[{
+			xtype : 'container',
+			width : 650,
+			height : 300,
+			layout : {
+				type : 'hbox',
+				align : 'stretch',
+				padding : 5
+				},
+			defaults : {
+				flex : 1
+				},
+			items :[ {
+				xtype : 'gridpanel',
+				name : 'firstGrid',
+				height : 100,
+				store : Ext.create('YAPP.store.Recursos'),
+				multiSelect : true,
+				viewConfig : {
+					plugins : {
+						ptype : 'gridviewdragdrop',
+						dragGroup : 'firstGridDDGroup',
+						dropGroup : 'secondGridDDGroup'
+						},
+					},
+				columns : columnas,
+				// stripeRows : true,
+				title : 'Recursos Disponibles',
+				margins : '0 2 0 0'
+				},
+				{
+					xtype : 'gridpanel',
+					name : 'secondGrid',
+					store : Ext.create('YAPP.store.Recursos'),
+					height : 100,
+					viewConfig : {
+						plugins : {
+							ptype : 'gridviewdragdrop',
+							dragGroup : 'secondGridDDGroup',
+							dropGroup : 'firstGridDDGroup'
+						},
+					},
+					columns : columnas,
+					// stripeRows : true,
+					title : 'Recursos asignados',
+					margins : '0 0 0 3'
+				} 
+			]
+		}]
 		this.layout = {
 			type : 'vbox',
 			align : 'stretch',
@@ -124,6 +98,34 @@ var columnas = [
 		field : {
 			type : 'textfield'
 		}
+	},
+	{
+		text: "Tipo",
+		flex : 1,
+		sortable : true,
+		width: 70,
+		renderer: renderizador_asignar_tipo,
+		field: {
+			type : 'textfield'
+		}
+	},
+	{
+		text: "Costo",
+		flex : 1,
+		sortable : true,
+		width: 70,
+		renderer: renderizador_asignar_costo,
+		field: {
+			type : 'textfield'
+		}
 	}
 ];
 
+function renderizador_asignar_costo(value, metaData, record) {
+	var costo = record.data._costo_hora + record.data._costo_cantidad;
+	return costo;
+}
+
+function renderizador_asignar_tipo(value, metaData, record) {
+	return record.data._tipo._tipo;
+}
